5c90a1641b5095ddd0670f3c7219a07cd089ad5a
DanielCordoba <dcordobap1@ucentral.edu.co>
2024-04-23 00:32:10 -0500

    se creo el repo

---
+++ src/main/java/ucentral/edu/co/ProyectoSoftware2/ProyectoSoftware2Application.java
@@ -0,0 +1,13 @@
+package ucentral.edu.co.ProyectoSoftware2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSoftware2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSoftware2Application.class, args);
+	}
+
+}
